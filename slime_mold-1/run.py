ae9c2bd77736e9badf6ace2ff0d5d052cef6ed7e
Robbie Koevoets <robbie.koevoets@live.nl>
2023-01-31 14:11:11 +0100

    small changes and put plot code in function

--- slime_mold-1/run.py
+++ slime_mold-1/run.py
@@ -7,6 +7,38 @@ from model import *
 from tokyo_mapping import *
 from analyze_graph import *
 
+
+def plot_result(graph, show_all=True, show_shortest=True):
+    fig = plt.figure(dpi=200, figsize=(3, 3))
+
+    if show_all:
+        # Plot all connections
+        for node, links in graph.items():
+            x0, y0 = node
+
+            for connected_coordinate, cost in links:
+                x, y = connected_coordinate
+                plt.plot([x0, x], [y0, y], color=(255/255, 242/255, 0, 1), lw=.5, zorder=-1)
+
+        x, y = zip(*graph.keys())
+        plt.scatter(x, y, color=(213/255, 184/255, 90/255, 1), s=.5)
+        x, y = zip(*model.food_locations.values())
+        plt.scatter(x, y, color='red', marker='s', s=3)
+
+    if show_shortest:
+        # Plot the shortest paths between the food sources
+        for path_key in shortest_paths:
+            path = shortest_paths[path_key]
+
+            for i in range(len(path) - 1):
+                (x0, y0) = path[i]
+                (x1, y1) = path[i + 1]
+
+                plt.plot([x0, x1], [y0, y1], color=(1, 0, 0, 1), lw=.5)
+
+    plt.show()
+
+
 size = 100
 p_branch = 0.075
 p_connect = 0.1
@@ -17,7 +49,6 @@ model = SlimeModel(size, size, p_branch, p_connect, signal_strength, noise)
 # cmap = cm.get_cmap('Greens')
 # norm = colors.Normalize(vmin = np.min(model.chem_values), vmax=np.max(model.chem_values))
 # image_colors = []
-fig = plt.figure(dpi=200, figsize=(3, 3))
 # # print(image_colors)
 # for x in range(size):
 #     image_colors.append([])
@@ -39,23 +70,20 @@ for i in range(200):
 # slime_x = []
 # slime_y = []
 
-convert_result_to_graph(model.connections, [model.food_locations[n] for n in model.food_locations])
-
+food_locations = [model.food_locations[n] for n in model.food_locations]
 graph = reduce_graph(model.connections, model.food_locations.values())
+nx_graph = convert_result_to_graph(graph)
+shortest_paths = get_all_shortest_paths(nx_graph, food_locations)
+
 # print(len(graph))
 # D = get_distance_matrix(model.food_locations)
 # raise NotImplementedError
 
-for node, links in graph.items():
-    x0, y0 = node
-    for connected_coordinate, cost in links:
-        x, y = connected_coordinate
-        plt.plot([x0, x], [y0, y], color=(255/255, 242/255, 0, 1), lw=.5, zorder=-1)
 
-x, y = zip(*graph.keys())
-plt.scatter(x, y, color=(213/255, 184/255, 90/255, 1), s=.5)
-x, y = zip(*model.food_locations.values())
-plt.scatter(x, y, color='red', marker='s', s=3)
+
+
+plot_result(graph)
+
 
 # for slime_cell in model.slime_cells:
 #     x0, y0 = slime_cell.origin
@@ -75,6 +103,4 @@ plt.scatter(x, y, color='red', marker='s', s=3)
 # print(vars(model.datacollector))
 # data = model.datacollector.get_model_vars_dataframe()
 
-
-plt.show()
 # print(vars(model.datacollector))
